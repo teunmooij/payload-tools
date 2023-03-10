28693398b27158522f56a826a91b833f4ca78c0c
Teun Mooij <tmooij@infinitaslearning.com>
2023-03-10 23:04:33 +0100

    update info

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Express } from 'express';
+import express, { Express } from 'express';
 import { SanitizedConfig } from 'payload/config';
 import swaggerUi from 'swagger-ui-express';
 import { createDocument } from './swagger';
@@ -9,5 +9,9 @@ export default async (app: Express, config: SanitizedConfig) => {
   const document = await createDocument(config);
 
   app.use('/api-specs', (req, res) => res.json(document));
+  if (document.info.license?.url) {
+    app.get('/api-docs/license', express.static('LICENSE'));
+  }
+
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(undefined, { swaggerUrl: '/api-specs' }));
 };
--- src/info.ts
+++ src/info.ts
@@ -1,5 +1,6 @@
-import { InfoObject } from 'openapi3-ts';
-import fs from 'fs/promises';
+import { InfoObject, LicenseObject } from 'openapi3-ts';
+import fsAsync from 'fs/promises';
+import fs from 'fs';
 import path from 'path';
 
 const defaultInfo: InfoObject = {
@@ -10,12 +11,23 @@ const defaultInfo: InfoObject = {
 export const getInfo = async (): Promise<InfoObject> => {
   try {
     const packageFilePath = path.join(process.cwd(), 'package.json');
-    const data = await fs.readFile(packageFilePath, 'utf-8');
-    const { name, version, description } = JSON.parse(data);
+    const data = await fsAsync.readFile(packageFilePath, 'utf-8');
+    const { name, version, description, license, swagger } = JSON.parse(data);
+    const hasLicenseFile = license && fs.existsSync(path.join(process.cwd(), 'LICENSE'));
+
+    const licenseInfo: LicenseObject | undefined = license
+      ? {
+          name: license,
+          url: hasLicenseFile ? '/api-docs/license' : undefined,
+        }
+      : undefined;
+
     return {
       title: name || defaultInfo.title,
       version: version || defaultInfo.version,
       description,
+      license: licenseInfo,
+      ...swagger,
     };
   } catch {
     return defaultInfo;
