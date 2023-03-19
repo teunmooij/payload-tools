00c7b7e1c257781648fdffe041e6177a213a02dd
teunmooij <tmooij@infinitaslearning.com>
2023-03-19 23:17:42 +0100

    replace testbed

---
+++ testbed/src/cron/reset.ts
@@ -0,0 +1,205 @@
+import payload from 'payload';
+import path from 'path';
+import fs from 'fs';
+import { User } from '../payload-types';
+import { MongoClient } from 'mongodb';
+import { generateContactFormSubmission, generateMailingListSubmission } from '../data/forms/submissionGenerator';
+import { homeData } from '../data/pages/homeData';
+import { homeDataDE } from '../data/pages/homeDataDE';
+import { homeDataES } from '../data/pages/homeDataES';
+import { videoSeriesData } from '../data/pages/videoSeriesData';
+import { caseStudiesData } from '../data/pages/caseStudiesData';
+import { contactFormData } from '../data/forms/contactFormData';
+import { mailingListFormData } from '../data/forms/mailingListFormData';
+import { generateTsInterfacesData } from '../data/posts/generateTsInterfacesData';
+import { whiteLabelAdminUIData } from '../data/posts/whiteLabelAdminUIData';
+import { buildWebsiteData } from '../data/posts/buildWebsiteData';
+import { introducingPayloadData } from '../data/posts/introducingPayloadData';
+import { futurePostData } from '../data/posts/futurePostData';
+import { mainMenuData } from '../data/mainMenu/mainMenuData';
+
+export async function seed() {
+  try {
+    payload.logger.info(`Seeding database...`);
+
+    const mediaDir = path.resolve(__dirname, '../../media');
+    if (fs.existsSync(mediaDir)) {
+      fs.rmSync(path.resolve(__dirname, '../../media'), { recursive: true });
+    }
+
+    await seedData();
+    payload.logger.info(`Seed Complete.`);
+  } catch (error) {
+    console.error(error);
+    payload.logger.error('Error seeding database.');
+  }
+}
+
+export async function reset() {
+  try {
+    payload.logger.info(`Resetting database...`);
+
+    const mediaDir = path.resolve(__dirname, '../../media');
+    if (fs.existsSync(mediaDir)) {
+      fs.rmSync(path.resolve(__dirname, '../../media'), { recursive: true });
+    }
+
+    await dropDB();
+    await seedData();
+    payload.logger.info(`Reset Complete.`);
+  } catch (error) {
+    console.error(error);
+    payload.logger.error('Error resetting database.');
+  }
+}
+
+async function dropDB() {
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  const db = client.db(new URL(process.env.MONGO_URL).pathname.substring(1));
+  await db.dropDatabase();
+}
+
+async function seedData() {
+  const { id: demoUserId } = await payload.create<User>({
+    collection: 'users',
+    data: {
+      name: 'Demo User',
+      email: 'demo@payloadcms.com',
+      password: 'demo',
+    },
+  });
+
+  const { id: imageId } = await payload.create<any>({
+    collection: 'media',
+    data: {
+      alt: 'Payload',
+    },
+    filePath: path.resolve(__dirname, './payload.jpg'),
+  });
+
+  // Page - Home
+  const homeString = homeData(imageId, demoUserId);
+  const homeStringDE = homeDataDE(imageId, demoUserId);
+  const homeStringES = homeDataES(imageId, demoUserId);
+
+  const { id: homeDocId } = await payload.create<any>({
+    collection: 'pages',
+    data: homeString,
+  });
+
+  // Page - Video Series
+  await payload.create<any>({
+    collection: 'pages',
+    data: videoSeriesData(imageId, demoUserId, homeDocId),
+  });
+
+  // Page - Case Studies
+  const { id: caseStudiesDocId } = await payload.create<any>({
+    collection: 'pages',
+    data: caseStudiesData(imageId, demoUserId, homeDocId),
+  });
+
+  // Main Menu
+  await payload.updateGlobal<any>({
+    slug: 'mainMenu',
+    data: mainMenuData(homeDocId, caseStudiesDocId),
+  });
+
+  // TEMPORARY - bug with breadcrumbs plugin. Home page resaves automatically after creation.
+  // I'm thinking that `de` simply got fired too early and overwritten.
+
+  setTimeout(async () => {
+    await payload.update({
+      collection: 'pages',
+      id: homeDocId,
+      locale: 'de',
+      data: homeStringDE,
+    });
+
+    await payload.update({
+      collection: 'pages',
+      id: homeDocId,
+      locale: 'es',
+      data: homeStringES,
+    });
+  }, 3000);
+
+  // Forms - Contact
+  const contactForm = await payload.create<any>({
+    collection: 'forms',
+    data: contactFormData(),
+  });
+  // Forms - Mailing List
+  const mailingListForm = await payload.create<any>({
+    collection: 'forms',
+    data: mailingListFormData(),
+  });
+
+  // Generate form submissions
+  const contactFormSubmissions = [...Array(5)].map(() => {
+    return payload.create<any>({
+      collection: 'form-submissions',
+      data: generateContactFormSubmission(contactForm.id),
+    });
+  });
+  await Promise.all(contactFormSubmissions);
+
+  const mailingListSubmissions = [...Array(5)].map(() => {
+    return payload.create<any>({
+      collection: 'form-submissions',
+      data: generateMailingListSubmission(mailingListForm.id),
+    });
+  });
+  await Promise.all(mailingListSubmissions);
+
+  // Create Categories
+  const [newsCategory, featureCategory, tutorialCategory] = await Promise.all([
+    payload.create<any>({
+      collection: 'categories',
+      data: {
+        name: 'news',
+      },
+    }),
+    payload.create<any>({
+      collection: 'categories',
+      data: {
+        name: 'feature',
+      },
+    }),
+    payload.create<any>({
+      collection: 'categories',
+      data: {
+        name: 'tutorial',
+      },
+    }),
+  ]);
+
+  await payload.create<any>({
+    collection: 'categories',
+    data: {
+      name: 'announcements',
+      archived: true,
+    },
+  });
+
+  await payload.create<any>({
+    collection: 'posts',
+    data: generateTsInterfacesData(demoUserId, featureCategory.id, imageId),
+  });
+  await payload.create<any>({
+    collection: 'posts',
+    data: whiteLabelAdminUIData(demoUserId, tutorialCategory.id, imageId),
+  });
+  await payload.create<any>({
+    collection: 'posts',
+    data: buildWebsiteData(demoUserId, tutorialCategory.id, imageId),
+  });
+  await payload.create<any>({
+    collection: 'posts',
+    data: introducingPayloadData(demoUserId, newsCategory.id, imageId),
+  });
+  await payload.create<any>({
+    collection: 'posts',
+    data: futurePostData(demoUserId, newsCategory.id, imageId),
+  });
+}
--- testbed/src/payload.config.ts
+++ testbed/src/payload.config.ts
@@ -1,20 +1,103 @@
 import { buildConfig } from 'payload/config';
-import path from 'path';
-import TodoLists from './collections/TodoLists';
-import Users from './collections/Users';
+import formBuilder from '@payloadcms/plugin-form-builder';
+import nestedDocs from '@payloadcms/plugin-nested-docs';
+import seo from '@payloadcms/plugin-seo';
 import { swagger } from 'payload-swagger';
+import path from 'path';
+import Categories from './collections/Categories';
+import Media from './collections/Media';
+import Posts from './collections/Posts';
+import Users from './collections/Users';
+import { Pages } from './collections/Pages';
+import MainMenu from './globals/MainMenu';
+import BeforeLogin from './components/BeforeLogin';
+import AfterDashboard from './components/AfterDashboard';
+import { Alerts } from './collections/Alerts';
+import BeforeDashboard from './components/BeforeDashboard';
 
+// the payload config is the entrypoint for configuring the entire application
+// all the API REST, GraphQL, authentication, file uploads, data layer and admin UI is built from the config
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
+  // the serverURL can be localhost:, a public domain or simply left undefined to work with relative
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   admin: {
+    // the user collection slug to use for authenticating to the admin panel, one per express app
     user: Users.slug,
+
+    // override existing payload styles with custom look
+    css: path.resolve(__dirname, './styles/custom.scss'),
+
+    // custom components added to show demo info
+    components: {
+      beforeLogin: [BeforeLogin],
+      beforeDashboard: [BeforeDashboard],
+      afterDashboard: [AfterDashboard],
+    },
   },
-  collections: [TodoLists, Users],
-  plugins: [swagger()],
+
+  // collections in Payload are synonymous with database tables, models or entities from other frameworks and systems
+  collections: [Categories, Media, Posts, Pages, Users, Alerts],
+
+  // globals are a single-instance collection, often used for navigation or site settings that live in one place
+  globals: [MainMenu],
+
+  // rateLimits provide basic API DDOS (Denial-of-service) protection and can limit accidental server load from scripts
+  rateLimit: {
+    trustProxy: true,
+    window: 2 * 60 * 1000, // 2 minutes
+    max: 2400, // limit each IP per windowMs
+  },
+
+  // GraphQL is included by default at /api/graphql
+  graphQL: {
+    disablePlaygroundInProduction: false,
+    disable: true,
+  },
+
+  // if not using graphQL it should be disabled for security and performance reasons
+  // graphQL: false
+
+  plugins: [
+    formBuilder({
+      formOverrides: {
+        admin: {
+          group: 'Content',
+        },
+      },
+      formSubmissionOverrides: {
+        admin: {
+          group: 'Admin',
+        },
+      },
+      redirectRelationships: ['pages', 'posts'],
+    }),
+    nestedDocs({
+      collections: ['pages'],
+      parentFieldSlug: 'parent',
+      breadcrumbsFieldSlug: 'breadcrumbs',
+      generateLabel: (_, doc) => doc.title as string,
+      generateURL: docs => docs.reduce((url, doc) => `${url}/${doc.slug}`, ''),
+    }),
+    seo({
+      collections: ['pages', 'posts'],
+    }),
+    swagger(),
+  ],
+
+  // optional customization of routes
+  routes: {
+    api: '/api',
+    admin: '/admin',
+    graphQL: '/graphql',
+    graphQLPlayground: '/graphql-playground',
+  },
+
+  localization: {
+    defaultLocale: 'en',
+    locales: ['en', 'es', 'de'],
+  },
+
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
-  },
-  graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
 });
