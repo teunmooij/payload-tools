9a5294feb343a86c1a81c64ab2cb9392bcc5c251
teunmooij <tmooij@infinitaslearning.com>
2023-03-20 10:06:06 +0100

    custom path basics

---
+++ src/payload-config/custom-paths.ts
@@ -0,0 +1,76 @@
+import type { OperationObject, PathItemObject, PathObject, SchemaObject } from 'openapi3-ts';
+import { Endpoint, SanitizedConfig } from 'payload/config';
+import { SanitizedCollectionConfig, SanitizedGlobalConfig } from 'payload/types';
+import { createResponse } from '../schemas';
+
+type Config = SanitizedConfig | SanitizedCollectionConfig | SanitizedGlobalConfig;
+type ConfigType = 'payload' | 'global' | 'collection';
+const isPayloadConfig = (config: Config): config is SanitizedConfig => !('slug' in config);
+
+const getBasePath = (config: Config, type: ConfigType) => {
+  if (isPayloadConfig(config)) return '';
+  if (type === 'global') return `/globals/${config.slug}`;
+  return `/${config.slug}`;
+};
+
+const getTags = (config: Config, type: ConfigType) => {
+  if (isPayloadConfig(config)) return ['custom'];
+  if (type === 'global') return [`global ${config.slug}`];
+  return [config.slug];
+};
+
+const operations: readonly (keyof PathItemObject)[] = ['get', 'head', 'post', 'put', 'patch', 'delete', 'options'] as const;
+const isOperation = (method: string): method is keyof PathItemObject => operations.includes(method as keyof PathItemObject);
+
+const setOperation = (pathItem: PathItemObject, operation: OperationObject, methods: Endpoint['method']) => {
+  const sanitizedMethod = methods.toLowerCase();
+
+  if (sanitizedMethod === 'all') {
+    setOperation(pathItem, operation, 'get');
+    setOperation(pathItem, operation, 'head');
+    setOperation(pathItem, operation, 'post');
+    setOperation(pathItem, operation, 'put');
+    setOperation(pathItem, operation, 'patch');
+    setOperation(pathItem, operation, 'delete');
+    setOperation(pathItem, operation, 'options');
+  }
+
+  if (isOperation(sanitizedMethod)) {
+    pathItem[sanitizedMethod] = operation;
+  }
+};
+
+export const getCustomPaths = (config: Config, type: ConfigType): PathObject => {
+  if (!config.endpoints?.length) return {};
+
+  const paths: PathObject = {};
+  const basePath = getBasePath(config, type);
+  const tags = getTags(config, type);
+
+  for (const endpoint of config.endpoints) {
+    // extract parameteres
+    const path = `${basePath}/${endpoint.path}`;
+    if (!paths[path]) paths[path] = {};
+
+    // determine summary, description, response
+    const responseSchema: SchemaObject | string = {
+      type: 'object',
+    };
+
+    const operation: OperationObject = {
+      tags,
+      description: 'custom operation',
+      responses: {
+        '200': createResponse('succesful operation', responseSchema),
+      },
+    };
+
+    if ((endpoint as Endpoint).root) {
+      operation.servers = [{ url: '' }];
+    }
+
+    setOperation(paths[path], operation, endpoint.method);
+  }
+
+  return paths;
+};
