e5b645bf52ff14aba92a77a68ccacb3a22ceca84
teunmooij <tmooij@infinitaslearning.com>
2023-03-22 20:38:25 +0100

    fix webpack

--- packages/swagger/src/plugin.ts
+++ packages/swagger/src/plugin.ts
@@ -24,7 +24,7 @@ export const swagger =
             },
             alias: {
               ...webpackConfig.resolve?.alias,
-              [path.resolve(__dirname, '../swagger')]: path.resolve(__dirname, 'utils/dummy-module'),
+              [path.resolve(__dirname, './swagger')]: path.resolve(__dirname, 'utils/dummy-module'),
             },
           },
         } as any;
