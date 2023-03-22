e87f2fd0912f9742aae1b1901d4dc097e0de26fd
teunmooij <tmooij@infinitaslearning.com>
2023-03-22 01:03:27 +0100

    should not publish

--- packages/testbed/src/components/BeforeDashboard/index.tsx
+++ packages/testbed/src/components/BeforeDashboard/index.tsx
@@ -6,14 +6,17 @@ import './index.scss';
 const baseClass = 'before-dashboard';
 
 const BeforeDashboard: React.FC = () => {
-	return (
-		<div className={baseClass}>
-			<Banner type="success">
-				<Check />
-				<strong>Payload is completely free and open-source.</strong> If you like what we're doing, <a href="https://github.com/payloadcms/payload" target="_blank">leave us a star on GitHub!</a>
-			</Banner>
-		</div>
-	);
+  return (
+    <div className={baseClass}>
+      <Banner type="success">
+        <Check />
+        <strong>Payload is completely free and open-source.</strong> If you like what we're doing,{' '}
+        <a href="https://github.com/payloadcms/payload" target="_blank">
+          leave us a star on GitHub!
+        </a>
+      </Banner>
+    </div>
+  );
 };
 
 export default BeforeDashboard;
