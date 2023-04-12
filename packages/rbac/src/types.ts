dd2c5267f4e0d6547bffd47ddf9667d8ee92588e
teunmooij <tmooij@infinitaslearning.com>
2023-04-12 09:18:38 +0200

    strong where type

--- packages/rbac/src/types.ts
+++ packages/rbac/src/types.ts
@@ -1,4 +1,6 @@
 import type { SelectField, Access as PayloadAccess, AccessArgs } from 'payload/types';
+import { Operator } from 'payload/types';
+import type { ObjectPaths, ValueAtPath, Join, Split, IsAny } from 'ts-powertypes';
 
 type NotNull<T> = T extends null ? never : T;
 
@@ -16,3 +18,55 @@ export interface Options {
   roles: SelectField['options'];
   defaultRoles?: string[];
 }
+
+type GreaterLessOperand<Field> = IsAny<Field> extends true
+  ? number | Date
+  : Field extends number
+  ? number
+  : Field extends Date
+  ? Date
+  : Field extends bigint
+  ? bigint
+  : never;
+
+type StringOperand<Field> = IsAny<Field> extends true ? string : Field extends string ? Field : never;
+
+type NearOperand<Field> = Field extends [number, number] ? [number | null | undefined, number | null | undefined] : never;
+
+type Operand<Field, O extends Operator> = O extends 'equals'
+  ? Field
+  : O extends 'not_equals'
+  ? Field
+  : O extends 'greater_than'
+  ? GreaterLessOperand<Field>
+  : O extends 'greater_than_equal'
+  ? GreaterLessOperand<Field>
+  : O extends 'less_than'
+  ? GreaterLessOperand<Field>
+  : O extends 'less_than_equal'
+  ? GreaterLessOperand<Field>
+  : O extends 'like'
+  ? StringOperand<Field>
+  : O extends 'contains'
+  ? StringOperand<Field>
+  : O extends 'in'
+  ? Field[]
+  : O extends 'not_in'
+  ? Field[]
+  : O extends 'exists'
+  ? boolean
+  : O extends 'near'
+  ? NearOperand<Field>
+  : never;
+
+type WhereField<Field> = {
+  [O in Operator]?: Operand<Field, O>;
+};
+type PathToString<T> = T extends any ? Join<T, '.'> : never;
+
+export type Where<T extends object = any> = {
+  [K in PathToString<ObjectPaths<T>>]?: WhereField<ValueAtPath<T, Split<K, '.'>>>;
+} & {
+  or?: Where<T>[];
+  and?: Where<T>[];
+};
